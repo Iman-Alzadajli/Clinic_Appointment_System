e398f574a644c727101ebaae6ece5ee06f67962d
ImanAdam331 <imanadamzedjali@gmail.com>
2025-05-08 12:26:15 +0400

    create classes : doctor, appointment,admin,patient

---
+++ Clinic_Appointment_System/Models/Admin.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clinic_Appointment_System.Models
+{
+    internal class Admin
+    {
+    }
+}
