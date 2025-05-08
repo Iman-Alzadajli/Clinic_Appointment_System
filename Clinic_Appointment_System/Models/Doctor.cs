f8d748b20a317559344bffaaa8cfce4be363b8d2
ImanAdam331 <imanadamzedjali@gmail.com>
2025-05-08 13:04:34 +0400

    create Patient and Doctors classes

--- Clinic_Appointment_System/Models/Doctor.cs
+++ Clinic_Appointment_System/Models/Doctor.cs
@@ -4,9 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+public enum Specialty
+{
+    Pediatrics,Cardiology,Dermatology
+}
+
 namespace Clinic_Appointment_System.Models
 {
-    internal class Doctor
+    public class Doctor
     {
+        public int Did { get; set; }
+        public string Name { get; set; }
+        public int Phone { get; set; }
+
+        public Specialty Specialty { get; set; }
+
+        public Doctor (int Did, string Name, int Phone , Specialty Specialty)
+        {
+            this.Did = Did;
+            this.Name = Name;
+            this.Phone = Phone;
+            this.Specialty = Specialty;
+        }
+         
     }
+
 }
--- Clinic_Appointment_System/Models/Patient.cs
+++ Clinic_Appointment_System/Models/Patient.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace Clinic_Appointment_System.Models
 {
-    internal class Patient
+    public class Patient
     {
+        public int Pid { get; set; }
+        public string Name { get; set; }
+        public int Phone {  get; set; }
+
+        public Patient (int Pid, string Name, int Phone)
+        {
+            this.Pid = Pid;
+            this.Name = Name;
+            this.Phone = Phone;
+        }
+
     }
 }
